9900fc0994315aaaeeb2a7f46aa8bcf424f2f2ff
Ifeoma Imoh <imoh_ifeoma@yahoo.co.uk>
2021-11-08 16:22:21 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Image, Transformation } from "cloudinary-react";
 
-function App() {
+const App = () => {
+  const [blogInput, setBlogInput] = useState("");
+  const [authorName, setAuthorName] = useState("");
+  const [displayInput, setDisplayInput] = useState(blogInput);
+  const [displayAutorName, setDisplayAutorName] = useState(authorName);
+
+  const renderOutput = (e) => {
+    e.preventDefault();
+    setDisplayInput(blogInput);
+    setDisplayAutorName(authorName);
+  };
+
+  function BlogTitle(blogInput, onTitleChange) {
+    return (
+      <div>
+        <label htmlFor="blog title">Blog Title:</label>
+        <input
+          value={blogInput}
+          onchange={onTitleChange}
+          type="text"
+          placeholder="Enter a blog title"
+        />
+      </div>
+    );
+  }
+
+  function Author(authorName, onNameChange) {
+    return (
+      <div>
+        <label htmlFor="Author's name">Author:</label>
+        <input
+          value={authorName}
+          onchange={onNameChange}
+          type="text"
+          placeholder="Enter author's name"
+        />
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="entire-form">
+        <div className="form-only">
+          <form onSubmit={renderOutput}>
+            <BlogTitle
+              title={blogInput}
+              onTitleChange={(e) => setBlogInput(e.target.value)}
+            />
+            <Author
+              authorName={blogInput}
+              onNameChange={(e) => setAuthorName(e.target.value)}
+            />
+            <button>Generate Social Image</button>
+          </form>
+        </div>
+        <div className="display">
+          <Image publicId={"social-card"} cloudName="ifeomaimoh">
+            <Transformation width="800" height="500" crop="fit" />
+            <Transformation
+              overlay={{
+                fontFamily: "Arial",
+                fontSize: 27,
+                text: displayInput,
+              }}
+              color="#FFFFFF"
+              width="500"
+              crop="fit"
+            />
+            <Transformation flags="layer_apply" gravity="west" x="40" />
+            <Transformation
+              overlay={{
+                fontFamily: "Arial",
+                fontSize: 27,
+                text: displayAutorName,
+              }}
+              color="#FFFFFF"
+              width="500"
+              crop="fit"
+            />
+          </Image>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
