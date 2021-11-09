7a54e2324aeddf2fc72d5fbf3333bea523b151ea
Ifeoma Imoh <imoh_ifeoma@yahoo.co.uk>
2021-11-09 15:07:23 +0400

    added field for author's name

--- src/App.js
+++ src/App.js
@@ -1,45 +1,46 @@
 import React, { useState } from "react";
 import { Image, Transformation } from "cloudinary-react";
 
+function BlogTitle({ blogInput, onTitleChange }) {
+  return (
+    <div>
+      <label htmlFor="blog title">Blog Title:</label>
+      <input
+        value={blogInput}
+        onChange={onTitleChange}
+        type="text"
+        placeholder="Enter a post title"
+      />
+    </div>
+  );
+}
+
+function Author({ authorName, onNameChange }) {
+  return (
+    <div>
+      <label>Author:</label>
+      <input
+        value={authorName}
+        onChange={onNameChange}
+        type="text"
+        placeholder="Enter author's name"
+      />
+    </div>
+  );
+}
+
 const App = () => {
   const [blogInput, setBlogInput] = useState("");
+  const [showBanner, setShowBanner] = useState(false);
+
   const [authorName, setAuthorName] = useState("");
-  const [displayInput, setDisplayInput] = useState(blogInput);
-  const [displayAutorName, setDisplayAutorName] = useState(authorName);
 
   const renderOutput = (e) => {
     e.preventDefault();
-    setDisplayInput(blogInput);
-    setDisplayAutorName(authorName);
+    if (blogInput && authorName) {
+      setShowBanner(true);
+    }
   };
-
-  function BlogTitle(blogInput, onTitleChange) {
-    return (
-      <div>
-        <label htmlFor="blog title">Blog Title:</label>
-        <input
-          value={blogInput}
-          onchange={onTitleChange}
-          type="text"
-          placeholder="Enter a blog title"
-        />
-      </div>
-    );
-  }
-
-  function Author(authorName, onNameChange) {
-    return (
-      <div>
-        <label htmlFor="Author's name">Author:</label>
-        <input
-          value={authorName}
-          onchange={onNameChange}
-          type="text"
-          placeholder="Enter author's name"
-        />
-      </div>
-    );
-  }
 
   return (
     <div className="wrapper">
@@ -47,42 +48,44 @@ const App = () => {
         <div className="form-only">
           <form onSubmit={renderOutput}>
             <BlogTitle
-              title={blogInput}
+              blogInput={blogInput}
               onTitleChange={(e) => setBlogInput(e.target.value)}
             />
             <Author
-              authorName={blogInput}
+              authorName={authorName}
               onNameChange={(e) => setAuthorName(e.target.value)}
             />
             <button>Generate Social Image</button>
           </form>
         </div>
-        <div className="display">
-          <Image publicId={"social-card"} cloudName="ifeomaimoh">
-            <Transformation width="800" height="500" crop="fit" />
-            <Transformation
-              overlay={{
-                fontFamily: "Arial",
-                fontSize: 27,
-                text: displayInput,
-              }}
-              color="#FFFFFF"
-              width="500"
-              crop="fit"
-            />
-            <Transformation flags="layer_apply" gravity="west" x="40" />
-            <Transformation
-              overlay={{
-                fontFamily: "Arial",
-                fontSize: 27,
-                text: displayAutorName,
-              }}
-              color="#FFFFFF"
-              width="500"
-              crop="fit"
-            />
-          </Image>
-        </div>
+        {showBanner && (
+          <div className="display">
+            <Image publicId={"social-card"} cloudName="ifeomaimoh">
+              <Transformation width="800" height="500" crop="fit" />
+              <Transformation
+                overlay={{
+                  fontFamily: "Arial",
+                  fontSize: 27,
+                  text: blogInput,
+                }}
+                color="#FFFFFF"
+                width="500"
+                crop="fit"
+              />
+              <Transformation flags="layer_apply" gravity="west" x="40" />
+              <Transformation
+                overlay={{
+                  fontFamily: "Arial",
+                  fontSize: 27,
+                  text: authorName,
+                }}
+                color="#FFFFFF"
+                width="500"
+                crop="fit"
+              />
+            </Image>
+          </div>
+        )}
       </div>
     </div>
   );
